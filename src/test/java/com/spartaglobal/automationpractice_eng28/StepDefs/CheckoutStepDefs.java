8b776102b5f91758572625ec00886f5c46fc7be1
T L Goddard <TGoddard@spartaglobal.com>
2019-05-03 16:37:49 +0100

    all checkout features completed

--- src/test/java/com/spartaglobal/automationpractice_eng28/StepDefs/CheckoutStepDefs.java
+++ src/test/java/com/spartaglobal/automationpractice_eng28/StepDefs/CheckoutStepDefs.java
@@ -7,25 +7,239 @@ import cucumber.api.java.en.When;
 
 public class CheckoutStepDefs {
 
-    //Scenario: I would like to add a delivery address
+    //Scenario: I would like to add a new address
     @Given("I am currently logged into the website")
     public void i_am_currently_logged_into_the_website() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
     @And("I have added the items I wanted to the basket")
     public void i_have_added_the_items_I_wanted_to_the_basket() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
     @And("I have clicked through the relevant pages to get to the checkout address page")
     public void i_have_clicked_through_the_relevant_pages_to_get_to_the_checkout_address_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
-    @When("I click on the add address button")
+    @When("I untick the use the delivery address as the billing address button")
+    public void i_untick_the_use_the_delivery_address_as_the_billing_address_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+    @And("I click on the add address button")
     public void i_click_on_the_add_address_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
-    @And("I have completed the {string} form")
-    public void i_have_completed_the_form(String string) {
+    @And("I have completed the your address form")
+    public void i_have_completed_the_your_address_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
-    @Then("The new address is available on the choose validity drop down box")
-    public void the_new_address_is_available_on_the_choose_validity_drop_down_box() {
+    @Then("The new address is available on the delivery address drop down box")
+    public void the_new_address_is_available_on_the_delivery_address_drop_down_box() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
     }
+    @And("The new address is available on the billing address drop down box")
+    public void the_new_address_is_available_on_the_billing_address_drop_down_box() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: I would like to update my billing address
+
+    @When("I untick the Use the delivery address as the billing address button")
+    public void i_untick_the_Use_the_delivery_address_as_the_billing_address_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+    @And("I click on the update billing address button")
+    public void i_click_on_the_update_billing_address_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+    @And("I have completed the changes to the your address form")
+    public void i_have_completed_the_changes_to_the_your_address_form() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+    @Then("The updated billing address is visible and displayed correctly")
+    public void the_updated_billing_address_is_visible_and_displayed_correctly() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: I would like to view the terms and conditions
+
+    @Given("I have clicked through the relevant pages to get to the checkout shipping page")
+    public void i_have_clicked_through_the_relevant_pages_to_get_to_the_checkout_shipping_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I click on the read terms of service button")
+    public void i_click_on_the_read_terms_of_service_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can view the terms and conditions")
+    public void i_can_view_the_terms_and_conditions() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: I would like to update my delivery address
+
+    @When("I click on the update delivery address button")
+    public void i_click_on_the_update_delivery_address_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+    @Then("The updated delivery address is visible and displayed correctly")
+    public void the_updated_delivery_address_is_visible_and_displayed_correctly() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: I would like to choose a different delivery address
+
+    @Given("I have previously created the address page that I want to use for delivery")
+    public void i_have_previously_created_the_address_page_that_I_want_to_use_for_delivery() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I click on the delivery dropdown box")
+    public void i_click_on_the_delivery_dropdown_box() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @And("I click on the address I want")
+    public void i_click_on_the_address_I_want() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the address that I want to be delivered to is displayed")
+    public void the_address_that_I_want_to_be_delivered_to_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: After reviewing my order in the final payment page I would like to continue shopping
+
+    @Given("I have clicked through the relevant pages to get to the checkout payment page")
+    public void i_have_clicked_through_the_relevant_pages_to_get_to_the_checkout_payment_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I click the continue shopping button")
+    public void i_click_the_continue_shopping_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I can see the shopping page")
+    public void i_can_see_the_shopping_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: I would like to pay by check
+
+    @When("I click on the pay by check button")
+    public void i_click_on_the_pay_by_check_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @And("I click on the confirm by order button")
+    public void i_click_on_the_confirm_by_order_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("The confirmation order message appears for check")
+    public void the_confirmation_order_message_appears_for_check() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: I would like to pay by bank wire
+
+    @When("I click on the pay by bank wire button")
+    public void i_click_on_the_pay_by_bank_wire_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("The confirmation order message appears")
+    public void the_confirmation_order_message_appears() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: I would like to add a comment to the order
+
+    @When("I click on the comment box")
+    public void i_click_on_the_comment_box() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @And("I type in the comment that I want to add")
+    public void i_type_in_the_comment_that_I_want_to_add() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("The comment has been added to the order")
+    public void the_comment_has_been_added_to_the_order() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+    //Scenario: I would like to continue with my purchase without accepting the terms and conditions
+
+    @Given("terms of service is unticked")
+    public void terms_of_service_is_unticked() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I click the proceed to checkout button")
+    public void i_click_the_proceed_to_checkout_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("A message appears to accept terms and conditions before proceeding")
+    public void a_message_appears_to_accept_terms_and_conditions_before_proceeding() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+
+
+
+
+
 
 
 
--- src/test/resources/features/Checkout.feature
+++ src/test/resources/features/Checkout.feature
@@ -1,9 +1,87 @@
 Feature: Checkout
 
-  Scenario: I would like to add a delivery address
+  Scenario: I would like to add a new address
     Given I am currently logged into the website
     And I have added the items I wanted to the basket
     And I have clicked through the relevant pages to get to the checkout address page
-    When I click on the add address button
-    And I have completed the "your address" form
-    Then The new address is available on the choose validity drop down box
+    When I untick the use the delivery address as the billing address button
+    And I click on the add address button
+    And I have completed the your address form
+    Then The new address is available on the delivery address drop down box
+    And The new address is available on the billing address drop down box
+
+
+  Scenario: I would like to update my billing address
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout address page
+    When I untick the Use the delivery address as the billing address button
+    And I click on the update billing address button
+    And I have completed the changes to the your address form
+    Then The updated billing address is visible and displayed correctly
+
+
+  Scenario: I would like to view the terms and conditions
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout shipping page
+    When I click on the read terms of service button
+    Then I can view the terms and conditions
+
+  Scenario: I would like to update my delivery address
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout address page
+    When I untick the use the delivery address as the billing address button
+    And I click on the update delivery address button
+    And I have completed the changes to the your address form
+    Then The updated delivery address is visible and displayed correctly
+
+  Scenario: I would like to choose a different delivery address
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout address page
+    And I have previously created the address page that I want to use for delivery
+    When I untick the Use the delivery address as the billing address button
+    And I click on the delivery dropdown box
+    And I click on the address I want
+    Then the address that I want to be delivered to is displayed
+
+  Scenario: After reviewing my order in the final payment page I would like to continue shopping
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout payment page
+    When I click the continue shopping button
+    Then I can see the shopping page
+
+  Scenario: I would like to pay by check
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout payment page
+    When I click on the pay by check button
+    And I click on the confirm by order button
+    Then The confirmation order message appears for check
+
+  Scenario: I would like to pay by bank wire
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout payment page
+    When I click on the pay by bank wire button
+    And I click on the confirm by order button
+    Then The confirmation order message appears
+
+  Scenario: I would like to add a comment to the order
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout address page
+    When I click on the comment box
+    And I type in the comment that I want to add
+    Then The comment has been added to the order
+
+  Scenario: I would like to continue with my purchase without accepting the terms and conditions
+    Given I am currently logged into the website
+    And I have added the items I wanted to the basket
+    And I have clicked through the relevant pages to get to the checkout shipping page
+    And terms of service is unticked
+    When I click the proceed to checkout button
+    Then A message appears to accept terms and conditions before proceeding
