2293acf53f9eda0f1e15c680c4607c1c9ff01922
Waine James <wjames@spartaglobal.com>
2019-05-07 12:23:47 +0100

    Added implementation to class

--- src/test/java/com/spartaglobal/automationpractice_eng28/AutomationPractice/Pages/RegistrationPage.java
+++ src/test/java/com/spartaglobal/automationpractice_eng28/AutomationPractice/Pages/RegistrationPage.java
@@ -1,5 +1,29 @@
 package com.spartaglobal.automationpractice_eng28.AutomationPractice.Pages;
 
-public class RegistrationPage
+import com.spartaglobal.automationpractice_eng28.AutomationPractice.KeyHandlers.KeyReader;
+import com.spartaglobal.automationpractice_eng28.AutomationPractice.KeyHandlers.KeySender;
+import org.openqa.selenium.WebDriver;
+
+
+/**
+ * KeyReader and KeySender provide access to the sendKeys and readKeys methods
+ */
+public class RegistrationPage implements KeyReader, KeySender
 {
+    private WebDriver driver;
+
+    public RegistrationPage(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+
+    @Override
+    public WebDriver getDriver()
+    {
+        return driver;
+    }
+
+
+
 }
