4f2c002d0cb0237863ada117b0d32451c86cd05e
Saheem Iqbal <Siqbal@spartaglobal.com>
2019-05-10 02:29:46 +0100

    All Quantity Stepdefs complete with unit tests. Changes to Feature file to decouple scenarios.

--- src/test/java/com/spartaglobal/automationpractice_eng28/AutomationPractice/Pages/BasketPage.java
+++ src/test/java/com/spartaglobal/automationpractice_eng28/AutomationPractice/Pages/BasketPage.java
@@ -3,7 +3,6 @@ package com.spartaglobal.automationpractice_eng28.AutomationPractice.Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,14 +19,20 @@ public class BasketPage {
     private By proceedToCheckoutLink = By.linkText("Proceed to checkout");
     private By popUpMenuID = By.id("layer_cart");
     private By getPopUpMenuClass = By.className("clearfix");
-    private By IncreaseQuantityButtonTitle = By.tagName("title");
+    private By IncreaseQuantityButtonPath = By.xpath("//*[@id=\"cart_quantity_up_1_1_0_0\"]");
+    private By decreaseQuantityButtonPath = By.xpath("//*[@id=\"cart_quantity_down_1_1_0_0\"]");
     private By productHomePageList = By.id("homefeatured");
     private By productHomePageListTagName = By.tagName("li");
     private By productBasketProductID = By.id("cart_summary");
     private By basketTableRows = By.tagName("tr");
+    private By basketProductQuantityBox = By.xpath("//*[@id=\"product_1_1_0_0\"]/td[5]/input[2]");
+    private By basketProductQuantityBoxClassName = By.name("quantity_1_1_0_0");
+    private By summaryProductQuantityID = By.id("summary_products_quantity");
+
 
     //Map for holding product table from home page
-    private Map<String,String> productDetails;
+    private Map<String,String> productHomepageDetails;
+    private Map<String,String> productBasketDetails;
     //List for holding list of products
     private List<WebElement> productList;
     private Products products;
@@ -48,10 +53,6 @@ public class BasketPage {
         return driver.getCurrentUrl();
     }
 
-    public void addSpecifiedProductToCart()
-    {
-
-    }
     private WebElement productTable(By tableName)
     {
         WebElement table = driver.findElement(tableName);
@@ -77,7 +78,6 @@ public class BasketPage {
     {
         driver.findElement(By.className("button-container")).findElement(continueShoppingLink).click();
     }
-
     public boolean checkPopUpMenuIsDisplayed()
     {
        return driver.findElement(getPopUpMenuClass).isDisplayed();
@@ -90,9 +90,7 @@ public class BasketPage {
 
     public boolean checkProductExistsInBasket()
     {
-        //System.out.println(productBasketTableData());
-      return productDetails.containsKey(driver.findElement(productBasketProductID).findElement(By.className("product-name")).getText());
-
+        return productHomepageDetails.containsKey(driver.findElement(productBasketProductID).findElement(By.className("product-name")).getText());
     }
     public void createProducts()
     {
@@ -105,34 +103,73 @@ public class BasketPage {
     public Map<String,String> productHomePageTableData()
     {
         List<WebElement> listOfProducts = productTable(productHomePageList).findElements(productHomePageListTagName);
-        productDetails = new HashMap<>();
+        productHomepageDetails = new HashMap<>();
         for (WebElement element : listOfProducts)
         {
-            productDetails.put(
+            productHomepageDetails.put(
                     element.findElement(By.className("product-name")).getText(),
                     element.findElement(By.className("right-block")).findElement(By.className("content_price")).getText()
             );
         }
-        return productDetails;
+        return productHomepageDetails;
     }
 
     public Map<String,String> productBasketTableData()
     {
-        List<WebElement> productTRList = productTable(productBasketProductID).findElement(By.tagName("tbody")).findElements(basketTableRows);
-        Map<String,String>  productDetails = new HashMap<>();
+        List<WebElement> productTRList = productTable(productBasketProductID).findElements(basketTableRows);
+        productBasketDetails = new HashMap<>();
         for(WebElement element : productTRList)
         {
-            productDetails.put(
-                    element.findElement(By.className("cart_description")).findElement(By.className("product-name")).getText(),
-                    element.findElement(By.className("cart-unit")).findElement(By.className("price")).getText());
+            productBasketDetails.put(
+                    element.findElement(By.className("product-name")).getText(),
+                    element.findElement(By.className("cart_unit")).findElement(By.className("price")).getText());
         }
-        System.out.println(productDetails.values());
-        return productDetails;
+        return productBasketDetails;
     }
 
     public void increaseQuantityButton()
     {
+        driver.findElement(IncreaseQuantityButtonPath).click();
+        driver.navigate().refresh();
+    }
 
+    public void decreaseQuantityButton()
+    {
+        driver.findElement(decreaseQuantityButtonPath).click();
+        driver.navigate().refresh();
+    }
+
+    public String getQuantityOfProduct()
+    {
+
+        return driver.findElement(basketProductQuantityBox).getAttribute("value");
+    }
+
+    public void inputSpecifiedQuantity(int inputValue)
+    {
+        driver.findElement(basketProductQuantityBoxClassName).clear();
+        driver.findElement(basketProductQuantityBoxClassName).sendKeys(Integer.toString(inputValue));
+    }
+
+    public String getQuantitySummary()
+    {
+       String summary =  driver.findElement(summaryProductQuantityID).getText();
+       String[] tokens = summary.split(" ");
+       return tokens[0];
+
+    }
+
+    public boolean checkIfCheckedOutOfBasketPage()
+    {
+        if(driver.getCurrentUrl().contains("http://automationpractice.com/index.php?controller=authentication&multi-shipping=0&display_guest_checkout=0&back=http%3A%2F%2Fautomationpractice.com%2Findex.php%3Fcontroller%3Dorder%26step%3D1%26multi-shipping%3D0"
+        )|| driver.getCurrentUrl().contains("http://automationpractice.com/index.php?controller=order&step=1"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public void quitDriver()
@@ -178,10 +215,6 @@ public class BasketPage {
             this.productPrice = productPrice;
         }
 
-//        public void setProductList(WebElement productList) {
-//            this.productList = productList;
-//        }
     }
-
 }
 
--- src/test/resources/features/Basket.feature
+++ src/test/resources/features/Basket.feature
@@ -1,16 +1,12 @@
 @BasketTest
 Feature: Basket
 
-
-  Scenario: Add a single product to the basket from the home page
+  Background: Add a single product to the basket from the home page and go to basket
     Given I am on the homepage
     When I click add to cart button
-    Then pop up menu appears saying product has been added to basket
-
-  Scenario: Go to Basket page after a product confirmed to have been added
-      Given I have the pop up menu opened
-      When I click proceed to checkout button
-      Then I am on the Basket Page
+    And pop up menu appears saying product has been added to basket
+    When I click proceed to checkout button
+    Then I am on the Basket Page
 
   Scenario: Increase Quantity of a product in the Basket
     Given I can see the product in the basket
@@ -29,21 +25,21 @@ Feature: Basket
 
   Scenario: Proceed to checkout
     Given I have a populated basket
-    When I click proceed to checkout
+    When I click proceed to checkout button
     Then I am on checkout
+  
+#  Scenario: Add multiple different products to the basket from the home page
+#    Given I am on the homepage
+#    When I click add to cart button for a product
+#    And pop up menu appears saying product has been added to the basket
+#    When I click continue shopping button
+#    And I click add to cart button for a different product
+#    Then pop up menu appears saying product has been added to the basket
 
-  Scenario: Add multiple different products to the basket from the home page
-    Given I am on the homepage
-    When I click add to cart button for a product
-    And pop up menu appears saying product has been added to the basket
-    When I click continue shopping button
-    And I click add to cart button for a different product
-    Then pop up menu appears saying product has been added to the basket
-
-  Scenario: Remove a single product from Basket
-    Given I am on the basket page
-    When I click on the delete icon
-    Then product is not displayed in basket
+#   Scenario: Remove a single product from Basket
+#    Given I am on the Basket Page
+#    When I click on the delete icon
+#    Then product is not displayed in basket
 
 
 # Further Scenarios if needed after First Test Flow Implemented
